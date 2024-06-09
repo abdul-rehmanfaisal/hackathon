21a61d6aea46ba2b6e9ef8cdc4231e733f78dc34
abdul-rehmanfaisal <154427514+abdul-rehmanfaisal@users.noreply.github.com>
2024-06-09 23:32:37 +0500

    Add files via upload

--- gemini.js
+++ gemini.js
@@ -6,6 +6,7 @@ const {
 const dialogflow = require('@google-cloud/dialogflow');
 const { WebhookClient, Payload } = require('dialogflow-fulfillment');
 const express = require("express");
+var nodemailer = require('nodemailer');
 const MODEL_NAME = "gemini-1.5-pro-latest";
 const API_KEY = "AIzaSyAwbaPFT8k16GmIOM3Xd-tbX-L8Q5N5Ss8";
 
@@ -68,14 +69,52 @@ webApp.post('/dialogflow', async (req, res) => {
             agent.add(result);
             console.log(result)
         }
+
+      
+
     }
     function hi(agent) {
         console.log(`intent  =>  hi`);
-        agent.add('Hi, I am your virtual assistant, Tell me how can I help you')
+        agent.add('Hi, I am your SMIT virtual assistant, Tell me  your name')
     }
+    function abcd(agent) {
+        const { phonenumber ,name1 ,nic,course,  age ,date,geocity,geocountry, email} = agent.parameters;
+       agent.add("email is sent and you are enrolled")
+    
+       var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'faisalabdulrehman97@gmail.com',
+    pass: 'xkjwpnpwxuxegzhm'
+  }
+});
+
+var mailOptions = {
+  from: 'faisalabdulrehman97@gmail.com',
+  to: 'asmaf610@gmail.com',
+  subject: 'Congratulations on Your Current Enrollment!',
+html:'<div style="border: 1000px;"><h1 style="text-align: center;">Congrats!</h1><img style="align-items: center; height: 10 rem;width: 30rem;" src="https://media.licdn.com/dms/image/D4D0BAQGD8npW7pZRLQ/company-logo_200_200/0/1684398542686?e=2147483647&v=beta&t=cxKn8F_pSgzn_3jv215dGJiM0ATx-S2EjSnxtJYR3XY"><p>Hi ${email} </p><br><p>I hope this email finds you in good spirits.</p><br><p style="text-align: center">Congratulations on being currently enrolled! Your commitment to your education is commendable, and I wanted to take a moment to acknowledge your dedication. Keep up the great work! If you ever need any assistance or have any questions, please donot hesitate to reach out. We are here to support you every step of the way.Wishing you continued success in your academic journey!</p></div>'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+
+     
+
+
+}
+    
     let intentMap = new Map();
     intentMap.set('Default Welcome Intent', hi);
     intentMap.set('Default Fallback Intent', fallback);
+    intentMap.set('smit-portal',abcd );
     agent.handleRequest(intentMap);
 });
 
